99559e627c41097cd39f339267c592a3ebbc1d08
Anselme Dor <anselmedor@hotmail.be>
2022-11-01 00:13:22 +0100

    Add carousel and animation to the header

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,3 +1,5 @@
+// adding google maps api
+
 function myMap() {
     // map options
     const options = {
@@ -12,6 +14,56 @@ function myMap() {
     const marker = new google.maps.Marker({
         position: {lat:42.4668,lng:-70.9495},
         map:map,
-        icon:'[../images/logo/gmap-logo.png'
+        // icon:'[../images/logo/gmap-logo.png]'
     });
 }
+
+// making the header carousel
+// declaring const
+const carousel = document.getElementById('carousel');
+const carouselBtnOne = document.getElementById('carousel__btn-one');
+const carouselBtnTwo = document.getElementById('carousel__btn-two');
+const carouselBtnThree = document.getElementById('carousel__btn-three');
+const carouselBtnFour = document.getElementById('carousel__btn-four');
+const firstTitle = document.getElementById('title__first-p');
+const secondTitle = document.getElementById('title__second-p')
+
+carouselBtnOne.addEventListener('click', () => {
+    carousel.style.backgroundImage = 'linear-gradient(0deg, rgb(27, 27, 27) 0%, rgba(0, 0, 0, 0) 100%),url(/resources/images/gallery/slide-1.jpg)';
+    carousel.classList.add('switchCarousel')
+    firstTitle.classList.add('transitionIn')
+    secondTitle.classList.add('transitionInDelay')
+});
+
+carouselBtnTwo.addEventListener('click', () => {
+    carousel.style.backgroundImage = 'linear-gradient(0deg, rgb(27, 27, 27) 0%, rgba(0, 0, 0, 0) 100%),url(/resources/images/gallery/slide-2.jpg)';
+    carousel.classList.add('switchCarousel')
+    firstTitle.classList.add('transitionIn')
+    secondTitle.classList.add('transitionInDelay')
+})
+
+carouselBtnThree.addEventListener('click', () => {
+    carousel.style.backgroundImage = 'linear-gradient(0deg, rgb(27, 27, 27) 0%, rgba(0, 0, 0, 0) 100%),url(/resources/images/gallery/slide-3.jpg)';
+    carousel.classList.add('switchCarousel')
+    firstTitle.classList.add('transitionIn')
+    secondTitle.classList.add('transitionInDelay')
+})
+
+carouselBtnFour.addEventListener('click', () => {
+    carousel.style.backgroundImage = 'linear-gradient(0deg, rgb(27, 27, 27) 0%, rgba(0, 0, 0, 0) 100%),url(/resources/images/gallery/slide-4.jpg)';
+    carousel.classList.add('switchCarousel')
+    firstTitle.classList.add('transitionIn')
+    secondTitle.classList.add('transitionInDelay')
+})
+
+carousel.addEventListener('animationend', () => {
+    carousel.classList.remove('switchCarousel')     // add the end of the animation, remove the animation
+})
+
+firstTitle.addEventListener('animationend', () => {
+    firstTitle.classList.remove('transitionIn')
+})
+
+secondTitle.addEventListener('animationend', () => {
+    secondTitle.classList.remove('transitionInDelay')
+})
