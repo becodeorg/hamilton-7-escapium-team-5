e6ee83eae4a641960413bdfe0a2b19a41f12f937
Anselme Dor <anselmedor@hotmail.be>
2022-11-04 15:15:11 +0100

    Adding small details

---
+++ resources/js/video.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('video__btn');
+const player = document.getElementById('video__youtube')
+
+btn.addEventListener('click', () => {
+    player.style.display = 'block'
+})
